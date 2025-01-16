241a47b70939bfd89835d6d881538f13fc238695
MZK <143114738+MZKvishnu@users.noreply.github.com>
2025-01-16 07:24:58 -1200

    Add files via upload

---
+++ shadow.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('#sun');
+let bdfullall = document.querySelector('.bdfullall');
+
+        function Animatedtoggle(){
+            btn.classList.toggle('active');
+            bdfullall.classList.toggle('active');
+        }
